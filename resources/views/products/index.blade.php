95d47074ffc62b34468ba05c0b902597b55c7f8c
FrancescoDale <fd060862@gmail.com>
2021-01-20 19:20:14 +0100

    stampa dei prodotti

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,1 +1,12 @@
 @extends('layouts.app')
+
+@section('content')
+    <h1 class=" text-center text-capitalize mt-4">inventario</h1>
+    @foreach ($products as $value)
+        <div class="container">
+            <p class="mt-4 text-capitalize">{{ $value->name }}</p>
+
+        </div>
+
+    @endforeach
+@endsection
