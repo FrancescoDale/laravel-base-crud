f46176163499dec3f047da2b07f1238a81fe3622
FrancescoDale <fd060862@gmail.com>
2021-01-21 14:46:57 +0100

    stampa dettagli prodotto

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -2,7 +2,14 @@
 
 @section('content')
     <div class="container">
-        <h1 class="text-center text-capitalize">view singola del prodotto : {{ $id }}</h1>
+        <h1 class="text-center text-capitalize">view singola del prodotto : {{ $product->id }}</h1>
+        <ul class="text-capitalize mt-4">
+            <li> {{ $product->name }} </li>
+            <li> {{ $product->color }} </li>
+            <li> {{ $product->price }} </li>
+            <li> {{ $product->size }} </li>
+            <li> {{ $product->description }} </li>
+        </ul>
 
     </div>
 @endsection
